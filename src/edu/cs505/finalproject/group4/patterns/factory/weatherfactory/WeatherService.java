434cd42da1c2cfc59f97476dbf10e7f7966dea74
Neelimaganti <32724761+Neelimaganti@users.noreply.github.com>
2018-11-23 22:40:53 -0500

    Added checked exception for the WeatherFactory package

--- src/edu/cs505/finalproject/group4/patterns/factory/weatherfactory/WeatherService.java
+++ src/edu/cs505/finalproject/group4/patterns/factory/weatherfactory/WeatherService.java
@@ -1,25 +1,26 @@
 package edu.cs505.finalproject.group4.patterns.factory.weatherfactory;
 
 import java.io.IOException;
+import java.text.ParseException;
 
-import org.json.JSONException;
 /**
- * 
+ *
  * WeatherService is an interface class which is extended by the concrete product
  * class. It will allow user to get the Weather data.
- * 
+ *
  * @version 1.0
  */
 public interface WeatherService
 {
 	/**
-	 * The method getWeatherData() will be implemented by the Weather 
-	 * Service Provider concrete class(OpenWebMapsWeatherService) to fetch 
+	 * The method getWeatherData() will be implemented by the Weather
+	 * Service Provider concrete class(OpenWebMapsWeatherService) to fetch
 	 * data from the Weather API based WEB_URL, CURRENT_URL, PARAM_CITY_NAME
 	 * ENCODING, PARAM_MODE, MODE, PARAMAPPID, APPID.
-	 * 
+	 *
 	 * @param inputs valid city name to fetch the data from OpenWeatherMap API.
 	 */
-	public abstract String getWeatherData(String inputs) throws IOException, JSONException;
-		
+	public String getWeatherData(String inputs) throws IOException, ParseException;
+
 }
+
--- src/edu/cs505/finalproject/group4/patterns/factory/weatherfactory/WeatherServiceFactory.java
+++ src/edu/cs505/finalproject/group4/patterns/factory/weatherfactory/WeatherServiceFactory.java
@@ -1,26 +1,25 @@
 package edu.cs505.finalproject.group4.patterns.factory.weatherfactory;
 
 
-/**
+**
  * The WeatherServiceFactory is an abstract Factory interface which defines
- * the Factory method and creates WeatherService object. 
- * 
+ * the Factory method and creates WeatherService object.
+ *
  * @version 1.0
  */
 
-public interface WeatherServiceFactory 
+public interface WeatherServiceFactory
 {
 
 	/**
 	 * The method getWeatherService() is a Factory method which can be implemented
-	 * by its subclass and gives an instance of the concrete WeatherService class. 
+	 * by its subclass and gives an instance of the concrete WeatherService class.
 	 * The implementation is done by the WeatherServiceCreatorFactory class
 	 *
 	 * @param sWeatherServiceType
+	 * @throws InvalidWeatherProviderException
 	 */
-	
-	public WeatherService getWeatherService(String sWeatherServiceType); 
+
+	public WeatherService getWeatherService(String sWeatherServiceType) throws InvalidWeatherProviderException;
 
 }
-
-
